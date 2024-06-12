c8d100be3babe196f154abbf02e2447044694ea9
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-06-12 23:29:17 +0300

    create project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div>
+      <p>Hello React</p>
+      <ul>
+        <li>sddd</li>
+        <li>ggf</li>
+        <li>gggh</li>
+        <li>dda</li>
+      </ul>
+      <p>Hello {username}</p>
+      <h1>Hello Tiny your test {username1}</h1>
+      <img src={Urlimg} alt="cat" />
+    </div>
+  );
+};
+const Urlimg =
+  "https://i.pinimg.com/originals/81/6d/a5/816da533638aee63cfbd315ea24cccbd.jpg";
+const username1 = 3 + 4 + 6;
+const username = "COrel Farell";
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
